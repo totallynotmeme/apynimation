29492eef3907622fe2a99d32760bde9d2f7f73a9
totally not a meme <210184278+totallynotmeme@users.noreply.github.com>
2026-09-05 11:14:05 +0300

    add Curve() + a few smaller stuff
    also this commit is heavily inflated cuz nvim stripped the spaces ;sob:

--- apynimation.py
+++ apynimation.py
@@ -12,17 +12,17 @@ class Input:
     mouse_buttons = [False] * 3
     mouse_just_pressed = [False] * 3
     mouse_just_released = [False] * 3
-    
+
     keyboard_keys = None
     shift = False
     ctrl = False
     alt = False
-    
+
     def update():
         Input.mouse_prev_pos.update(Input.mouse_pos)
         Input.mouse_pos.update(pg.mouse.get_pos())
         Input.mouse_rel.update(Input.mouse_pos - Input.mouse_prev_pos)
-        
+
         new_buttons = pg.mouse.get_pressed()
         Input.mouse_just_pressed = [
             new_buttons[0] and not Input.mouse_buttons[0],
@@ -35,7 +35,7 @@ class Input:
             not new_buttons[2] and Input.mouse_buttons[2],
         ]
         Input.mouse_buttons = new_buttons
-        
+
         Input.keyboard_keys = pg.key.get_pressed()
         mods = pg.key.get_mods()
         Input.shift = bool(mods & pg.KMOD_SHIFT)
@@ -50,15 +50,15 @@ class Window:
     is_open = False
     surface = None
     clock = None
-    
+
     scene = None
     global_objects = []
     event_map = {}
-    
+
     fps = 60 # default
     dt = 1/60
     t = 0
-    
+
     def create(size, caption="Untitled window", **kwargs):
         # surely nothing will break if you try to call create() multiple times
         pg.init()
@@ -66,16 +66,16 @@ class Window:
         Window.surface = pg.display.set_mode(size, **kwargs)
         Window.clock = pg.time.Clock()
         Window.is_open = True
-    
+
     def close(ev=None): # ev argument used for event handling
         Window.is_open = False
         pg.quit()
-    
+
     def set_fps(fps):
         Window.fps = fps
         Window.dt = 1/fps
         return Window.dt
-    
+
     def finish_frame():
         # rendering
         Window.clear()
@@ -85,19 +85,19 @@ class Window:
             obj.update(Window.t)
             obj.render(Window.surface)
         Window.post()
-        
+
         pg.display.flip()
         Window.clock.tick(Window.fps)
-        
+
         # handling stuff
         Window.t += Window.dt
         if Window.scene is not None:
             Window.scene.t += Window.dt
-        
+
         Input.update()
         for ev in pg.event.get():
             Window.event_map.get(ev.type, DO_LITERALLY_NOTHING)(ev)
-    
+
     def add_event_handler(new_map, _nowarn=False):
         Window.event_map.update(new_map)
         if pg.QUIT in new_map and not _nowarn:
@@ -107,11 +107,11 @@ If you're writing your own handler, make sure to call Window.close() when you're
 Add _nowarn=True parameter to remove this warning
 """[1:-1]
             print(msg)
-    
+
     # utility functions that can be overwritten to create special effects
     def clear(): # runs before the frame is drawn
         Window.surface.fill("black")
-    
+
     def post(): # runs after the frame is drawn, unused by default
         pass
 
@@ -124,14 +124,14 @@ class Scene:
         self.t = 0
         self.size = size
         self.layers = []
-    
+
     def __repr__(self):
         return f"<{self.__class__.__name__} {len(self.layers)} layers>"
-    
+
     def render(self, target):
         for layer in self.layers:
             layer.render(target, t=self.t)
-    
+
     def create_layer(self):
         a = Layer(self.size)
         self.layers.append(a)
@@ -143,27 +143,29 @@ class Layer:
     def __init__(self, size):
         self.objects = []
         self.surf = pg.Surface(size, pg.SRCALPHA)
-    
+
     def __repr__(self):
         return f"<{self.__class__.__name__} {len(self.objects)} objects>"
-    
+
     def render(self, target, t=0):
         self.clear()
-        
+
         for obj in self.objects:
             obj.update(t)
             obj.render(self.surf)
-        
+
         self.blit(target)
-    
-    def add(self, obj):
-        self.objects.append(obj)
-        return obj
-    
+
+    def add(self, *objects):
+        self.objects.extend(objects)
+        if len(objects) == 1:
+            return objects[0]
+        return objects
+
     # utility functions that can be overwritten to create layer-specific effects
     def clear(self):
         self.surf.fill((0, 0, 0, 0))
-    
+
     def blit(self, target):
         target.blit(self.surf, (0, 0))
 
@@ -173,13 +175,13 @@ class Point:
     def __init__(self, x=0, y=0, data=None):
         self.pos = pg.Vector2(x, y)
         self.data = data or {}
-    
+
     def __repr__(self):
         return f"<{self.__class__.__name__} @ {self.pos}>"
-    
+
     def update(self, t=0):
         pass
-    
+
     def render(self, target):
         pixel_pos = (int(self.pos.x), int(self.pos.y))
         target.set_at(pixel_pos, "white")
@@ -188,24 +190,24 @@ class Point:
 class Point3d:
     # NOTE: this DOES NOT handle cases when the point is behind the camera
     # If the point is behind, it will end up being flipped back to the front
-    
+
     def __init__(self, x=0, y=0, z=0, data=None):
         self.pos3d = pg.Vector3(x, y, z)
         self.pos = pg.Vector2()
         self.prev_t = -1
         self.data = data or {}
-        
+
         self.update() # to set self.pos
-    
+
     def __repr__(self):
         return f"<{self.__class__.__name__} @ {self.pos3d}>"
-    
+
     def update(self, t=0):
         # micro-optimization to prevent it from recomputing the same point 100 times
         if self.prev_t == t:
             return
         self.prev_t = t
-        
+
         focal_len = self.data["focal_length"]
         pos3d = self.pos3d - self.data["camera_pos"]
         x = (pos3d.x * focal_len) / (pos3d.z + focal_len)
@@ -213,7 +215,7 @@ class Point3d:
         x += self.data["win_size"].x / 2
         y += self.data["win_size"].y / 2
         self.pos.update(x, y)
-    
+
     def render(self, target):
         pixel_pos = (int(self.pos.x), int(self.pos.y))
         target.set_at(pixel_pos, "white")
@@ -226,14 +228,14 @@ class Line:
         self.p2 = p2
         self.width = width
         self.color = pg.Color(color)
-    
+
     def __repr__(self):
         return f"<{self.__class__.__name__}  {self.p1} -- {self.p2}>"
-    
+
     def update(self, t=0):
         self.p1.update(t)
         self.p2.update(t)
-    
+
     def render(self, target):
         pg.draw.line(target, self.color, self.p1.pos, self.p2.pos, self.width)
 
@@ -244,14 +246,14 @@ class Wireframe:
         self.color = pg.Color(color)
         self.width = width
         self.closed = closed
-    
+
     def __repr__(self):
         return f"<{self.__class__.__name__} {len(self.points)} points>"
-    
+
     def update(self, t=0):
         for i in self.points:
             i.update(t)
-    
+
     def render(self, target):
         positions = [i.pos for i in self.points]
         pg.draw.lines(target, self.color, self.closed, positions, self.width)
@@ -262,14 +264,14 @@ class Polygon:
         self.points = points
         self.width = width
         self.color = pg.Color(color)
-    
+
     def __repr__(self):
         return f"<{self.__class__.__name__} {len(self.points)} points>"
-    
+
     def update(self, t=0):
         for i in self.points:
             i.update(t)
-    
+
     def render(self, target):
         positions = [i.pos for i in self.points]
         pg.draw.polygon(target, self.color, positions, self.width)
@@ -285,14 +287,14 @@ class Rect:
         self.color = color
         self.width = width # bruh
         self.rect = pg.Rect(0, 0, 0, 0)
-    
+
     def __repr__(self):
         if self.p2 is None:
             suffix = f"{self.w}x{self.h}"
         else:
             suffix = f"-- {self.p2}"
         return f"{self.__class__.__name__}  {self.p1} {suffix}"
-    
+
     def update(self, t=0):
         if self.p2 is None:
             self.p1.update(t)
@@ -308,10 +310,10 @@ class Rect:
             self.w = right - left
             self.h = bottom - top # 0 is up, 100 is down
         self.rect.update(self.x, self.y, self.w, self.h)
-    
+
     def render(self, target):
         pg.draw.rect(target, self.color, self.rect, self.width)
-    
+
     def collidepoint(self, point):
         return self.rect.collidepoint(point)
 
@@ -325,20 +327,20 @@ class Circle:
         self.width = width
         self.color = pg.Color(color)
         self.update()
-    
+
     def __repr__(self):
         return f"<{self.__class__.__name__} @ {self.center} r={self.radius:.2f}px>"
-    
+
     def update(self, t=0):
         self.center_point.update(t)
         self.center = self.center_point.pos
         if self.radius_point is not None:
             self.radius_point.update(t)
             self.radius = self.center.distance_to(self.radius_point.pos)
-    
+
     def render(self, target):
         pg.draw.circle(target, self.color, self.center, self.radius, self.width)
-    
+
     def collidepoint(self, point): # name based on pygame.Rect.collidepoint
         return self.center.distance_to(point) <= self.radius
 
@@ -348,7 +350,7 @@ class CircleNgon(Circle):
         super().__init__(*args, **kwargs)
         self.sides = sides
         self.angle = angle
-    
+
     def render(self, target):
         up = pg.Vector2(0, -self.radius)
         angle_step = 360 / self.sides
@@ -365,18 +367,23 @@ class Sprite: # (pg.sprite.Sprite)
         self.point = point
         self.surface = surface
         self.align = align
-    
+        self.rect = pg.Rect(0, 0, 0, 0)
+
     def __repr__(self):
         return f"<{self.__class__.__name__} @ {self.pos}>"
-    
+
     def update(self, t=0):
         if self.point is not None:
             self.point.update(t)
             self.pos.update(self.point.pos)
-    
+
     def render(self, target):
         pos = self.surface.get_rect(**{self.align: self.pos})
-        target.blit(self.surface, pos)
+        new_rect = target.blit(self.surface, pos)
+        self.rect.update(new_rect)
+
+    def collidepoint(self, point):
+        return self.rect.collidepoint(point)
 
 
 pg.font.init() # required for fonts to work
@@ -384,12 +391,12 @@ class Text(Sprite):
     def __init__(self, font, text="[...]", color="white", **kwargs):
         surface = font.render(text, True, color)
         super().__init__(surface=surface, **kwargs)
-        
+
         self.text = text
         self.font = font
         self.color = color
         self._prev_render = (self.font, self.text, self.color)
-    
+
     def update(self, t=0):
         super().update(t)
         this_render = (self.font, self.text, self.color)
@@ -404,16 +411,16 @@ class Ticker:
         self.time = time
         self.wrap = wrap
         self._val = _val
-    
+
     def __repr__(self):
         return f"Ticker(time={self.time} wrap={self.wrap} _val={self._val})"
-    
+
     def step(self, dt=None):
         self._val += dt or Window.dt
-        
+
         if self._val < self.time:
             return False # not yet
-        
+
         if self.wrap:
             self._val -= self.time
         else: # reset the whole thing
@@ -425,62 +432,62 @@ class Tape:
     """
     [[placeholder docstring]]
     an infinite 'tape' of looping values that can be cycled through.
-    
+
     example usage:
     a = Tape([1, 2, 3])
     for _ in range(6):
-        print(a.next(), end="; ") # 1; 2; 3; 1; 2; 3; 
+        print(a.next(), end="; ") # 1; 2; 3; 1; 2; 3;
     print()
     for _ in range(5):
-        print(a.prev(), end="; ") # 2; 1; 3; 2; 1; 
+        print(a.prev(), end="; ") # 2; 1; 3; 2; 1;
     print()
-    
+
     NOTE: this can be imitated with itertools.cycle() using:
     a = itertools.cycle([1, 2, 3])
     for _ in range(6):
-        print(next(a), end="; ") # 1; 2; 3; 1; 2; 3; 
+        print(next(a), end="; ") # 1; 2; 3; 1; 2; 3;
     print()
     # .prev() function cannot be recreated this way as far as i know
     """
-    
+
     def __init__(self, elements=None, _ind=-1):
         self.elements = elements or []
         self.ind = _ind
-    
+
     def __repr__(self):
         return f"<Tape x{len(self.elements)} elements ind={self.ind}>"
-    
+
     # these two can probably be merged into a generic .shift()
     # but i find .prev() and .next() more readable
     def prev(self, n=1):
         if not self.elements:
             return None
-        
+
         if self.ind == -1:
             self.ind = len(self.elements) - 1
         else:
             self.ind -= n
             self.ind %= len(self.elements)
-        
+
         return self.elements[self.ind]
-    
+
     def next(self, n=1):
         if not self.elements:
             return None
-        
+
         if self.ind == -1:
             self.ind = 0
         else:
             self.ind += n
             self.ind %= len(self.elements)
-        
+
         return self.elements[self.ind]
-    
+
     def set(self, element):
         if element not in self.elements:
             self.ind = -1
             return
-        
+
         self.ind = self.elements.index(element)
 
 
@@ -488,7 +495,7 @@ class Limiter:
     """
     [[placeholder docstring]]
     a helper class to ratelimit events
-    
+
     example usage:
     a = Limiter(0.5) # time in seconds
     def callback(ev):
@@ -496,19 +503,44 @@ class Limiter:
             print("you just left clicked! you can left click again in 0.5s")
     Window.add_event_handler({pg.MOUSEBUTTONDOWN: callback})
     """
-    
+
     def __init__(self, cooldown, _last_call=None):
         self.cooldown = cooldown
         self.last_call = _last_call or -999999999 # float("-inf") doesn't exist
-    
+
     def __repr__(self):
         return f"<Limiter every {self.cooldown}s>"
-    
+
     def call(self):
         dt = Window.t - self.last_call
-        
+
         if 0 <= dt < self.cooldown:
             return False
-        
+
         self.last_call = Window.t
         return True
+
+
+class Curve:
+    """
+    [[placeholder docstring]]
+    """
+
+    def __init__(self, points):
+        self.points = points
+        self.update_points()
+
+    def update_points(self):
+        self.points.sort(key=lambda vec: vec.pos.x)
+
+    def get(self, val):
+        if val <= self.points[0].pos.x:
+            return self.points[0].pos.y
+        if val >= self.points[-1].pos.x:
+            return self.points[-1].pos.y
+
+        for prev_p, next_p in zip(self.points, self.points[1:]):
+            if val < next_p.pos.x:
+                dist = next_p.pos.x - prev_p.pos.x
+                f = (val - prev_p.pos.x) / dist
+                return next_p.pos.y * f + prev_p.pos.y * (1-f)
